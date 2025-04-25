9473731097a1f30d6044ea9c5474df4b0b6f5488
DJ Cartwright <dj.cartwright.web@gmail.com>
2025-04-25 12:52:33 -0400

    Initial Commit

---
+++ FirebaseTutorials/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseTutorials
+//
+//  Created by David Cartwright on 2025-04-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseTutorials/FirebaseTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTutorialsApp.swift
+//  FirebaseTutorials
+//
+//  Created by David Cartwright on 2025-04-25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
